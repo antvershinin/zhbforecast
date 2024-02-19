8ccc70d88f46c7a8f3ed374b920a891ff37687c4
Anton Vershinin <a.vershinin92@gmail.com>
2024-02-19 10:20:46 +0300

    added form

--- client/src/api/matchesApi.tsx
+++ client/src/api/matchesApi.tsx
@@ -6,3 +6,12 @@ export const getEuroMatches = async () => {
   const result = await AxiosInstance.get("/matches/euro/getmatches");
   return result.data;
 };
+
+export const setEuroForecastsDB = async (
+  matches: [{ score1: number; score2: number }]
+) => {
+  const result = await AxiosInstance.put("/matches/euro/setforecast", {
+    matches,
+  });
+  return result.status;
+};
--- client/src/pages/eurocuppage/EurocupPage.tsx
+++ client/src/pages/eurocuppage/EurocupPage.tsx
@@ -3,60 +3,86 @@ import { getEuroMatches } from "../../api/matchesApi";
 import { EuroTable } from "../../components/tables/euro/tables/EuroTable";
 import { EuroMatchesShow } from "../../components/tables/euro/matches/EuroMatchesShow";
 import { ForecastList } from "../../components/tables/euro/forecasts/ForecastsList";
+import { EuroForecastForm } from "../../components/forms/forms";
 
-export interface IEuroTable  {
-user_name : string, forecast_points : number, exact : number, difference : number, outcome : number
+export interface IEuroTable {
+  user_name: string;
+  forecast_points: number;
+  exact: number;
+  difference: number;
+  outcome: number;
 }
 
 export interface IEuroMatch {
-  is_double : boolean, score1 : number, score2 : number, teams : [string, string]
+  is_double: boolean;
+  score1: number;
+  score2: number;
+  teams: [string, string];
 }
 
 export interface IEuroForecasts {
-  user_name : string, user_score : [], user_forecast : [{score1: number, score2: number}], forecast_points: number
+  user_name: string;
+  user_score: [];
+  user_forecast: [{ score1: number; score2: number }];
+  forecast_points: number;
 }
 
 interface ITour {
-  canMakeForecast? : boolean,
-  tour : {
-    forecasts : IEuroForecasts[],
-    matches : IEuroMatch[],
-    table : IEuroTable[]
-  }
+  canMakeForecast?: boolean;
+  tour: {
+    forecasts: IEuroForecasts[];
+    matches: IEuroMatch[];
+    table: IEuroTable[];
+  };
 }
 
-
-
 export const EurocupPage = () => {
-  
-  
-
-  const [data, setData] = useState<ITour>()
-
+  const [data, setData] = useState<ITour>();
 
   useEffect(() => {
     const getData = async () => {
       const response = await getEuroMatches();
-      setData(response)
+      setData(response);
     };
 
     getData();
   }, []);
 
-  const doubleMatch = data?.tour.matches.findIndex((el)=> el.is_double)
+  const doubleMatch = data?.tour.matches.findIndex((el) => el.is_double);
 
-  return <> {data ?  
-    <div style={{display:'flex', flexDirection:'column', alignItems:'center', gap: '50px'}}> 
-    <div>
-  <EuroTable data={data.tour.table}/> 
-    </div>
-    <div>
-    <EuroMatchesShow matches={data.tour.matches}/>
-    </div>
-    <div>
-      <ForecastList double_match={doubleMatch} forecasts={data.tour.forecasts}/>
-    </div>
-    </div>
-  : <>No data</>}</>
-
+  return (
+    <>
+      {" "}
+      {data ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "50px",
+          }}
+        >
+          <div>
+            <EuroTable data={data.tour.table} />
+          </div>
+          <div>
+            <EuroMatchesShow matches={data.tour.matches} />
+          </div>
+          <div>
+            <ForecastList
+              double_match={doubleMatch}
+              forecasts={data.tour.forecasts}
+            />
+          </div>
+          <div>
+            {!data.canMakeForecast ? null : (
+              <EuroForecastForm matches={data.tour.matches} />
+            )}
+          </div>
+        </div>
+      ) : (
+        <>No data</>
+      )}
+    </>
+  );
 };
