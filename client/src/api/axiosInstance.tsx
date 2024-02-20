6f8ce6d836933761bca0d406b0395e91ca3fb6bb
Anton Vershinin <a.vershinin92@gmail.com>
2024-02-20 08:35:17 +0300

    fixed axiosInstance

--- client/src/api/axiosInstance.tsx
+++ client/src/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 export const AxiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "http://62.217.180.248:8000/api",
 });
 
 AxiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
